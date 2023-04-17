30283099d4b121adc20e661d9111619df9822c5f
YeffersonDelgado <yeffersondaviddega@ufps.edu.co>
2023-04-17 10:33:48 -0500

    Commit Inicial

---
+++ src/main/java/primerParcialWeb/modelo/Paciente.java
@@ -0,0 +1,29 @@
+package primerParcialWeb.modelo;
+
+import java.sql.Date;
+
+public class Paciente {
+
+	private Integer id;
+	
+	private Integer documento;
+	
+	private String nombre;
+	
+	private String apellido;
+	
+	private String email;
+	
+	private String genero;
+	
+	private Date fechaNacimiento;
+	
+	private String telefono;
+	
+	private String direccion;
+	
+	private Integer peso;
+	
+	private Integer estatura;
+	
+}
